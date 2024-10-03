42b0fa2ca5c06bdb16b5540132cab818612acafb
Eric Grahn <eric.grahn@chasacademy.se>
2024-10-03 14:03:02 +0200

    Test så att jag fattar

--- test.c
+++ test.c
@@ -3,8 +3,6 @@
 int main () {
     
     printf("Hello world!");
-    printf("Test för att se om jag lyckas få in detta till Master branch");
-    printf("Test för att se om jag lyckas få in detta tillbaka till datorn");
 
     return 0;
 }
